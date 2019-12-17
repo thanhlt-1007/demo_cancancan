6c4d7a419b3598fce02d517cd1024ceb8ed00fc4
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-12-17 19:55:29 +0700

    add_gem_devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class type
+    case type
+    when "notice"
+      "alert-success"
+    when "alert"
+      "alert-danger"
+    else
+      "alert-#{type}"
+    end
+  end
 end
