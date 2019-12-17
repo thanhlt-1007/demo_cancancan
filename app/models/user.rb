39fd6768b6d59238b46371904ed7af9319f3e43a
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-12-17 20:51:48 +0700

    update_view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable, :rememberable
   devise :database_authenticatable, :registerable, :validatable
+
+  validates :password_confirmation, presence: true, if: :password
 end
